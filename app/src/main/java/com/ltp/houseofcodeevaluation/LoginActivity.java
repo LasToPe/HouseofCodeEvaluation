e8fe1c602d49e2b01a4facf83fb6d19b67a5a04e
Lasse Pedersen <lassep95@gmail.com>
2019-11-17 16:07:51 +0100

    Added project files, splash screen done, login started

---
+++ app/src/main/java/com/ltp/houseofcodeevaluation/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.ltp.houseofcodeevaluation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setupAuth();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 123) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if(resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            } else {
+                // TODO show dialog of some kind...
+            }
+        }
+    }
+
+    private void setupAuth() {
+        AuthUI.IdpConfig p[] = new AuthUI.IdpConfig[] {
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build()
+        };
+
+        List<AuthUI.IdpConfig> providers = Arrays.asList(p);
+
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), 123);
+    }
+}
